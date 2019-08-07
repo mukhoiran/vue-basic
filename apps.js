28bd513e4b84b86c6ce95ca7fead7c8d66fa2e89
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-07 17:17:53 +0700

    output combination

--- apps.js
+++ apps.js
@@ -1,8 +1,8 @@
 var app = new Vue({
   el:'#app',
   data:{
-    message: 'Hallo vue!',
-    // message: '<b>Hallo</b> <i>vue!</i> <u>I am here</u>',
+    // message: 'Hallo vue!',
+    message: '<b>Hallo</b> <i>vue!</i> <u>I am here</u>',
     message2: 'Halo Javascript',
     message3: 'Halo HTML',
   }
