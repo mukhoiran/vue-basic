25552ae43fbf31edcdf46bf1f631c23ded024f10
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-07 17:24:46 +0700

    directive rendering another data type

--- apps.js
+++ apps.js
@@ -2,8 +2,14 @@ var app = new Vue({
   el:'#app',
   data:{
     // message: 'Hallo vue!',
-    message: '<b>Hallo</b> <i>vue!</i> <u>I am here</u>',
-    message2: 'Halo Javascript',
-    message3: 'Halo HTML',
+    // message: '<b>Hallo</b> <i>vue!</i> <u>I am here</u>',
+    // message2: 'Halo Javascript',
+    // message3: 'Halo HTML',
+    text: 'hallo this is text',
+    number1: 100,
+    number2: 10.3,
+    choice1: true,
+    choice2: false,
+    array: ['one','two','three']
   }
 })
